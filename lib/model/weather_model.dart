e19b7e5c4b283e5275d74ead2298dc2df408516b
AthiraaAthi <athiraa2612@gmail.com>
2024-03-04 11:11:40 +0530

    initial commit

---
+++ lib/model/weather_model.dart
@@ -0,0 +1,9 @@
+class WeatherModel {
+  final String cityName;
+  final double temperature;
+  final String mainCondition;
+  WeatherModel(
+      {required this.cityName,
+      required this.temperature,
+      required this.mainCondition});
+}
