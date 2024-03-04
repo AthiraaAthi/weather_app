74583b999826b2fcfec227c0f60f0085366e07d6
AthiraaAthi <athiraa2612@gmail.com>
2024-03-04 11:18:04 +0530

    created model class

--- lib/model/weather_model.dart
+++ lib/model/weather_model.dart
@@ -6,4 +6,11 @@ class WeatherModel {
       {required this.cityName,
       required this.temperature,
       required this.mainCondition});
+
+  factory WeatherModel.fromJson(Map<String, dynamic> json) {
+    return WeatherModel(
+        cityName: json["name"],
+        temperature: json["main"]["temp"].toDouble(),
+        mainCondition: json["weather"][0]["main"]);
+  }
 }
